e248be65ce00bd6c009d1c40d90b58d32aa52395
Chirag Bansal <cban@netlight.com>
2023-09-04 12:12:27 +0200

    Update test with pagination

--- src/test/java/com/s2/todo/backend/service/TodoServiceTest.java
+++ src/test/java/com/s2/todo/backend/service/TodoServiceTest.java
@@ -1,12 +1,12 @@
 package com.s2.todo.backend.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 import com.s2.todo.backend.model.Todo;
 import com.s2.todo.backend.repository.TodoRepository;
-import com.s2.todo.backend.service.TodoService;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -17,6 +17,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 
@@ -34,18 +37,24 @@ public class TodoServiceTest {
 
     @Test
     public void testGetAllTodos() {
-        // Mock the repository to return a list of TODOs
-        when(todoRepositoryMock.findAll()).thenReturn(Arrays.asList(
+        // Create a list of TODOs
+        List<Todo> todoList = Arrays.asList(
             Todo.builder().description("Task 1").status(Todo.Status.NOT_DONE).build(),
             Todo.builder().description("Task 2").status(Todo.Status.DONE).build()
-        ));
+        );
 
-        List<Todo> todos = todoService.getAllTodos();
+        // Create a Page object from the list (simulating pagination)
+        Page<Todo> todoPage = new PageImpl<>(todoList);
+
+        // Mock the repository to return the Page object
+        when(todoRepositoryMock.findAll(any(Pageable.class))).thenReturn(todoPage);
+
+        Page<Todo> todos = todoService.getAllTodos(1, 10); // Assuming page 1 and 10 items per page
 
         // Assertions
-        assertThat(todos.size()).isEqualTo(2);
-        assertThat(todos.get(0).getDescription()).isEqualTo("Task 1");
-        assertThat(todos.get(1).getStatus()).isEqualTo(Todo.Status.DONE);
+        assertThat(todos.getTotalElements()).isEqualTo(2);
+        assertThat(todos.getContent().get(0).getDescription()).isEqualTo("Task 1");
+        assertThat(todos.getContent().get(1).getStatus()).isEqualTo(Todo.Status.DONE);
     }
 
     @Test
@@ -64,15 +73,24 @@ public class TodoServiceTest {
 
     @Test
     public void testGetNotDoneTodos() {
-        // Arrange
-        List<Todo> notDoneTodos = new ArrayList<>();
-        when(todoRepositoryMock.findByStatus(Todo.Status.NOT_DONE)).thenReturn(notDoneTodos);
+        // Create a list of "not done" TODOs
+        List<Todo> notDoneTodoList = Arrays.asList(
+            Todo.builder().description("Task 1").status(Todo.Status.NOT_DONE).build(),
+            Todo.builder().description("Task 2").status(Todo.Status.NOT_DONE).build()
+        );
 
-        // Act
-        List<Todo> result = todoService.getNotDoneTodos();
+        // Create a Page object from the list (simulating pagination)
+        Page<Todo> notDoneTodoPage = new PageImpl<>(notDoneTodoList);
 
-        // Assert
-        assertThat(result).isEqualTo(notDoneTodos);
+        // Mock the repository to return the Page object
+        when(todoRepositoryMock.findByDone(eq(false), any(Pageable.class))).thenReturn(notDoneTodoPage);
+
+        Page<Todo> notDoneTodos = todoService.getNotDoneTodos(1, 10); // Assuming page 1 and 10 items per page
+
+        // Assertions
+        assertThat(notDoneTodos.getTotalElements()).isEqualTo(2);
+        assertThat(notDoneTodos.getContent().get(0).getDescription()).isEqualTo("Task 1");
+        assertThat(notDoneTodos.getContent().get(1).getDescription()).isEqualTo("Task 2");
     }
 
     @Test
